90fb67a57e83bb2b1bdd389476f8f5f3b884fc28
Massart Nathan <220747@CCM-Asty-Moulin.local>
2022-10-13 13:56:25 +0200

    premiere version formulaire

---
+++ Formulaire1/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+</head>
+<body>
+    <form action="">
+        <fieldset>
+            <legend> Vos coordonnés</legend>
+            <div>
+                <label for="nom"> Nom :</label>
+                <input type="text" name="nom" id="nom">
+            </div>
+            <div>
+                <label for="Prénom"> Prénom :</label>
+                <input type="text" name="Prénom" id="Prénom">
+            </div>
+            <div>
+                <label for="Date de naissance"> Date de naissance :</label>
+                <input type="text" name="Date de naissance" id="date de naissance">
+            </div>
+            <div>
+                <label for="Email"> Email :</label>
+                <input type="text" name="Email" id="Email">
+            </div>
+            <div>
+                <label for="Pays">Pays :</label>
+                <select name="pays" id="pays">
+                    <optgroup label="Europe">
+                        <option value="France">France</option>
+                        <option value="Belgique">Belgique</option>
+                        <option value="Pays-Bas">Pays-Bas</option>
+                    </optgroup>
+                </select>
+                
+            </div>
+        </fieldset>
+        <fieldset>
+            <legend>Périodicité</legend>
+            
+        </fieldset>
+        <fieldset>
+            <legend>Version papier/ informatique</legend>
+        </fieldset>
+    </form>
+</body>
+</html>
